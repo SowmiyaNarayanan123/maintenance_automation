08b434fc8516354de73266df8c850b36a292afbb
SowmiyaNarayanan123 <snnarayanan195@gmail.com>
2025-05-21 15:40:15 +0530

    initial setup

---
+++ src/main/java/com/nectar/maintenance_automation/MaintenanceAutomationApplication.java
@@ -0,0 +1,14 @@
+package com.nectar.maintenance_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceAutomationApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/nectar/maintenance_automation/MaintenanceAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nectar.maintenance_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
